02f28e12660d9feab62dd5e48e7085d0e1927ef6
vishalSBhat <vishalbhat97@gmail.com>
2020-07-13 21:05:38 +0530

    Updates

---
+++ precache-manifest.c057c88f451a274825da22886836da25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb65685e87aa7b8b669a5ec46d6a072",
+    "url": "/note-it/index.html"
+  },
+  {
+    "revision": "5443888c0ffe3a9c96a4",
+    "url": "/note-it/static/css/main.6fe07b9a.chunk.css"
+  },
+  {
+    "revision": "05526dbf780d38204a9d",
+    "url": "/note-it/static/js/2.600d360f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/note-it/static/js/2.600d360f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5443888c0ffe3a9c96a4",
+    "url": "/note-it/static/js/main.ee0b0ec6.chunk.js"
+  },
+  {
+    "revision": "43be8e579737738c1b55",
+    "url": "/note-it/static/js/runtime-main.e5770e57.js"
+  },
+  {
+    "revision": "c8b2f7af34c1a104018cf7962275fd94",
+    "url": "/note-it/static/media/error.c8b2f7af.jpg"
+  },
+  {
+    "revision": "dc896a6f683cc8e52ee7eb1fe2278351",
+    "url": "/note-it/static/media/loading.dc896a6f.svg"
+  }
+]);
